8024480196ee33276ed0a9a9d241aba6ee5bf7d1
chris <perrella.chris@gmail.com>
2022-04-05 23:48:57 -0700

    Add a template plugin for the shelf

---
+++ plugin/sat_adap_tex/sat_adap_tex/__init__.py
@@ -0,0 +1,104 @@
+import os, weakref
+from functools import partial
+from pathlib import Path
+from PySide2 import QtCore, QtGui, QtWidgets, QtSvg
+
+import sd
+
+def icon_svg(name, size):
+    currentDir = os.path.dirname(__file__)
+    icon_path = os.path.abspath(os.path.join(currentDir, name + '.svg'))
+    icon_path_hover = os.path.abspath(os.path.join(currentDir, name + '_hover.svg'))
+    
+    if not Path(icon_path_hover).is_file():
+        icon_path_hover = icon_path    
+
+    svgRenderer = QtSvg.QSvgRenderer(icon_path)
+    svgRenderer_hover = QtSvg.QSvgRenderer(icon_path_hover)
+    if svgRenderer.isValid():
+        pixmap = QtGui.QPixmap(QtCore.QSize(size, size))
+        pixmap_hover = QtGui.QPixmap(QtCore.QSize(size, size))
+
+        if not pixmap.isNull():
+            pixmap.fill(QtCore.Qt.transparent)
+            painter = QtGui.QPainter(pixmap)
+            svgRenderer.render(painter)
+            painter.end()
+
+            pixmap_hover.fill(QtCore.Qt.transparent)
+            painter_hover = QtGui.QPainter(pixmap_hover)
+            svgRenderer_hover.render(painter_hover)
+            painter_hover.end()            
+
+        icon = QtGui.QIcon()
+        icon.addPixmap(pixmap)
+        icon.addPixmap(pixmap_hover, QtGui.QIcon.Active)
+        return icon
+
+    return None
+
+class AdaptiveTexturingShelf(QtWidgets.QToolBar):
+    __toolbarList = {}
+
+    def __init__(self, graphViewID, ui_mrg):
+        super(AdaptiveTexturingShelf, self).__init__(parent=ui_mrg.getMainWindow())
+
+        self.__graphViewID = graphViewID
+        self.__uiMgr = ui_mrg
+
+        act = self.addAction(icon_svg("generate_hex", 64), "Generate Hex")
+        act.setToolTip(self.tr("Generate Hex"))
+        act.triggered.connect(self.__onGenerateHex)
+
+        self.__toolbarList[graphViewID] = weakref.ref(self)
+        self.destroyed.connect(partial(AdaptiveTexturingShelf.__onToolbarDeleted, graphViewID=graphViewID))
+
+
+    def tooltip(self):
+        return self.tr("Adaptive Texturing")
+            
+    def __onGenerateHex(self):
+        print('Generate Hex')
+
+
+    @classmethod
+    def __onToolbarDeleted(cls, graphViewID):
+        del cls.__toolbarList[graphViewID]
+
+    @classmethod
+    def removeAllToolbars(cls):
+        for toolbar in cls.__toolbarList.values():
+            if toolbar():
+                toolbar().deleteLater()
+
+
+def onNewGraphViewCreated(graphViewID, ui_mrg):
+    toolbar = AdaptiveTexturingShelf(graphViewID, ui_mrg)
+    ui_mrg.addToolbarToGraphView(
+        graphViewID,
+        toolbar,
+        icon = icon_svg("adaptive_texturing", 64),
+        tooltip = toolbar.tooltip())
+
+graphViewCreatedCallbackID = 0
+
+def initializeSDPlugin():
+    ctx = sd.getContext()
+    app = ctx.getSDApplication()
+    ui_mrg = app.getQtForPythonUIMgr()
+
+    if ui_mrg:
+        global graphViewCreatedCallbackID
+        graphViewCreatedCallbackID = ui_mrg.registerGraphViewCreatedCallback(
+            partial(onNewGraphViewCreated, ui_mrg=ui_mrg))
+
+
+def uninitializeSDPlugin():
+    ctx = sd.getContext()
+    app = ctx.getSDApplication()
+    ui_mrg = app.getQtForPythonUIMgr()
+
+    if ui_mrg:
+        global graphViewCreatedCallbackID
+        ui_mrg.unregisterCallback(graphViewCreatedCallbackID)
+        AdaptiveTexturingShelf.removeAllToolbars()
