53920b5b8edb30e8723c90d83ebae8a50340584a
chris <perrella.chris@gmail.com>
2022-04-11 23:43:44 -0700

    Add a function to organize a graph layout

--- satadap/graph_cmds.py
+++ satadap/graph_cmds.py
@@ -1,11 +1,89 @@
-import glob, getpass
+import glob, getpass, ctypes
 from pathlib import Path
-from os.path import relpath
+from grandalf.layouts import SugiyamaLayout
+from grandalf.graphs import Vertex, Edge, Graph
 
 from satadap import bake_cmds
 
 from pysbs import sbsarchive, sbsgenerator
 from pysbs.api_exceptions import SBSImpossibleActionError
+
+
+class NodeHierarchy(object):
+	def __init__(self, graph):
+		super(NodeHierarchy, self).__init__()
+		self.nodes = []
+		self.nodes = graph.getNodeList()
+	
+	def get_edges(self):
+		edges = []
+		for x, node in enumerate(self.nodes):
+			for connected_node in node.getConnectedNodesUID():
+				for y, other_node in enumerate(self.nodes):
+					if other_node.mUID == connected_node:
+						edges.append((x, y))
+		return edges
+
+
+class NodeGraph(Graph):
+	def __init_(self):
+		super(NodeGraph, self).__init__()
+	
+	def get_graph_center(self, sub_graph_index):
+		pos = [v.view.xy for v in self.C[sub_graph_index].sV]
+		center = (sum(x[0] for x in pos) / len(pos), sum(x[1] for x in pos) / len(pos))
+		return center
+
+
+class VertPosition(object):
+	def __init__(self, node):
+		self.node = node
+		self.w, self.h = 144, 144
+		pos = self.node.getPosition()
+		self.xy = float2(pos[1], -pos[0])
+
+
+class float2(ctypes.Structure):
+	_fields_ = [('x', ctypes.c_float), ('y', ctypes.c_float)]
+
+	def __str__(self):
+		return 'x: %f, y: %f' % (self.x, self.y)
+
+	def __getitem__(self, index):
+		if index == 0:
+			return self.x
+		elif index == 1:
+			return self.y
+		else:
+			raise IndexError('Index out of range')
+
+
+def layout_graph( sbs_graph ):
+	hierarchy = NodeHierarchy(sbs_graph)	
+	vertices = [Vertex(n) for n in range(len(hierarchy.nodes))]
+	edges = [Edge(vertices[v], vertices[w]) for (v, w) in hierarchy.get_edges()]
+	graph = NodeGraph(vertices, edges)
+
+	for v in vertices:
+		v.view = VertPosition(hierarchy.nodes[v.data])
+	
+	for subgraph in range(len(graph.C)):
+		center = graph.get_graph_center(subgraph)
+		roots = list([x for x in graph.C[subgraph].sV if len(x.e_in()) == 0])
+		
+		layout = SugiyamaLayout(graph.C[subgraph])        
+		layout.init_all(roots=roots)
+		layout.draw()
+		new_center = graph.get_graph_center(subgraph)
+		
+		if len(hierarchy.nodes) == 1:
+			pos = hierarchy.nodes[roots[0].data].getPosition()
+			offset = float2(pos[1] - roots[0].view.xy[0], -pos[0] - roots[0].view.xy[1])
+		else:
+			offset = float2(center[0] - new_center[0], center[1] - new_center[1])
+
+		for i, v in enumerate(graph.C[subgraph].sV):
+			v.view.node.setPosition([-v.view.xy[1] - offset[1], v.view.xy[0] + offset[0], 0])
 
 
 def set_graph_attribute( graph, attribute_dict ):
@@ -104,7 +182,7 @@ def create_model_graph_outputs( graph, multi_material_node, output_node_dict ):
 	material_config = bake_cmds.get_material_config()	
 	for output in material_config['Outputs'][0]:
 		output_node = graph.createOutputNode( output.lower() )
-		node_connect( graph, multi_material_node, output, output_node, 'inputNodeOutput' )
+		#node_connect( graph, multi_material_node, output, output_node, 'inputNodeOutput' )
 		output_node_dict[output] = output_node
 	
 	return output_node_dict
@@ -132,10 +210,19 @@ def create_model_graph( sbs_context, sbsdoc_path, mesh_dict, bake_model=True ):
 
 	# Composite the baked normal map with the composited materials
 	normal_combine_node_url = 'sbs://normal_combine.sbs'
-	normal_combine_node = graph.createCompInstanceNodeFromPath( document, normal_combine_node_url, aParameters={ 'blend_quality': 2 } )
+	normal_combine_node_params = { 'blend_quality': 2 }
+	normal_combine_node = graph.createCompInstanceNodeFromPath( document, normal_combine_node_url, aParameters=normal_combine_node_params )
 	node_connect( graph, multi_material_node, 'Normal', normal_combine_node, 'Input' )
 	node_connect( graph, resource_node_dict['Normal Map from Mesh'], 'output', normal_combine_node, 'Input_1' )
+	
+	# Add the output combiner node
+	output_combiner_node_url = 'utilities://utility_output_combiner.sbs'
+	output_combiner_node = graph.createCompInstanceNodeFromPath( document, output_combiner_node_url )
+	for output in output_node_dict:
+		node_connect( graph, multi_material_node, output, output_combiner_node, output)
+		node_connect( graph, output_combiner_node, output, output_node_dict[output], 'inputNodeOutput')
 	node_connect( graph, normal_combine_node, 'output', output_node_dict['Normal'], 'inputNodeOutput' )
 
+	layout_graph( graph )
 	document.writeDoc( str(sbsdoc_path) )
 	bake_cmds.cook_sbsar( sbs_context, sbsdoc_path ) 
